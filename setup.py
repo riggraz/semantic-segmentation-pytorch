0900d98971fbaac5b0022550bf79461b7369874b
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2021-12-09 13:07:00 +0100

    Update setup information

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='mit_semseg',
+    name='riggraz_semseg',
     version='1.0.0',
     author='MIT CSAIL',
     description='Pytorch implementation for Semantic Segmentation/Scene Parsing on MIT ADE20K dataset',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/CSAILVision/semantic-segmentation-pytorch',
+    url='https://github.com/riggraz/semantic-segmentation-pytorch',
     packages=setuptools.find_packages(),
     classifiers=(
         'Programming Language :: Python :: 3',
